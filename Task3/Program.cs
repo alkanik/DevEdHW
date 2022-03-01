1a90301a2d0b7036b3f078faca46e94881c42873
alkanik <alkanik@gmail.com>
2022-03-01 22:15:53 +0300

    task 3

---
+++ Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Пользователь вводит 1 число (A).
+// Найдите количество положительных целых чисел, квадрат которых меньше A.
+
+
+Console.WriteLine("Введите целое положительное число А:");
+int a = Convert.ToInt32(Console.ReadLine());
+int number = 0;
+
+for (int i = 1; i*i < a; i++)
+{
+    number++;
+}
+Console.WriteLine($"Ответ: {number}");
