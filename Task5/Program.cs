4ca549af5497fcbf662db5ceb753989052f677fb
alkanik <alkanik@gmail.com>
2022-03-01 23:12:25 +0300

    task 5

---
+++ Task5/Program.cs
@@ -0,0 +1,30 @@
+﻿// Пользователь вводит 2 числа (A и B).
+// Вывести сумму всех чисел из диапазона от A до B, которые делятся без остатка на 7.
+// (Учтите, что при вводе B может оказаться меньше A).
+
+Console.WriteLine("Введите число A:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B:");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int tmp = 0;
+int result = 0;
+
+if (a>b)
+{
+    tmp = a;
+    a = b;
+    b = tmp;
+}
+
+int number = a;
+
+for (int i=a; i<=b; i++)
+{
+    if (i%7==0)
+    {
+        result += i;
+    }
+}
+
+Console.WriteLine($"Результат:{result}");
