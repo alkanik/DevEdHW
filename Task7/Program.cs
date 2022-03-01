5921b38d3c2693ce4ffe03c780393624c21d6a2f
alkanik <alkanik@gmail.com>
2022-03-01 23:55:17 +0300

    task7

---
+++ Task7/Program.cs
@@ -0,0 +1,21 @@
+﻿// Пользователь вводит 2 числа.
+// Найти их наибольший общий делитель используя алгоритм Евклида.
+
+Console.WriteLine("Введите первое число:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int b = Convert.ToInt32(Console.ReadLine());
+
+while (a != b)
+{
+    if (a > b)
+    {
+        a = a - b;
+    }
+    else
+    {
+        b = b - a;
+    }
+}
+
+Console.WriteLine(a);
