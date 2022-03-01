6593634f28cd9a88e09941894aebb0443629fdfb
alkanik <alkanik@gmail.com>
2022-03-01 22:30:05 +0300

    task 4

---
+++ Task4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Пользователь вводит 1 число (A).
+// Вывести наибольший делитель (кроме самого A) числа A.
+
+Console.WriteLine("Введите целое положительное число A:");
+int a = Convert.ToInt32(Console.ReadLine());
+int divisor = 0;
+
+for (int i = 1; i < a; i++)
+{
+    if (a%i==0)
+    {
+        divisor = i;
+    }
+}
+Console.WriteLine(divisor);
