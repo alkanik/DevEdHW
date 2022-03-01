dafab1b15e55c88af23af33fa6f0ab32d7dbf3ad
alkanik <alkanik@gmail.com>
2022-03-01 22:05:24 +0300

    task 2

---
+++ Task2/Program.cs
@@ -0,0 +1,11 @@
+﻿// Пользователь вводит 1 число (A). Вывести все числа от 1 до 1000, которые делятся на A.
+
+Console.WriteLine("Введите целое число А от 0 до 1000:");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Числа, которые делятся на {a} без остатка:");
+
+for (int i = a; i <= 1000; i += a)
+{
+    Console.WriteLine(i);
+}
