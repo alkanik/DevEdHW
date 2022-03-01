68b05e1cee73a6a8e4b81be454d5e20ad3817c95
alkanik <alkanik@gmail.com>
2022-03-01 21:50:15 +0300

    task 1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,3 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Пользователь вводит 2 числа (A и B). Возвести число A в степень B.
 
+Console.WriteLine("Введите целое положительное число А:");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите целое положительное число B:");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int tmp = 1;
+int result = 1;
+
+while(tmp <= b)
+{
+    result = result * a;
+    tmp++;
+}
+Console.WriteLine("A в степени B равно " + result);
